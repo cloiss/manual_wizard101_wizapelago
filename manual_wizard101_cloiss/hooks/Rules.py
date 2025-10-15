2b25f5816fa560f8d4276226118983f28e37eefc
ItzGray <63925441+ItzGray@users.noreply.github.com>
2025-10-15 16:31:31 -0500

    Implemented better Universal Tracker integration (#33)
    * Update Rules.py
    
    * Updated canReachLocation to new custom function
    
    * Updated canReachLocation to new custom function
    
    * Update Rules.py
    
    * Fixed Photomancy 1 conditional
    
    * Fix Shopping halloween logic
    
    * Handle majid port
    
    * Fix SD logic for friendly
    
    * clean up and fix logic, remove duplicate logic by referencing wizReach in all cases (renamed from wizReachLocation) (NOT TESTED)
    
    * simplify wizReach using strings + minor logic fixes
    
    ---------
    
    Co-authored-by: Andrew Sonnier <absonnie@ncsu.edu>
    Co-authored-by: cloiss <epikcloiss37@gmail.com>
    Co-authored-by: Cloiss <70874037+cloiss@users.noreply.github.com>

--- manual_wizard101_cloiss/hooks/Rules.py
+++ manual_wizard101_cloiss/hooks/Rules.py
@@ -1,10 +1,24 @@
 from typing import Optional
 from worlds.AutoWorld import World
-from ..Helpers import clamp, get_items_with_value
+from ..Helpers import clamp, get_items_with_value, get_option_value
 from BaseClasses import MultiWorld, CollectionState
 
 import re
 
+def wizReach(location: str) -> bool:
+    locations_dict = {
+        # "$y" references the logic for "y" elsewhere in this table using recursion
+        # "x OR y OR z" will return true if any of "x", "y", or "z" are true (can be used with any number of args)
+        # Note this list is incomplete; it only has the locations that are necessary for the randomizer to work
+        "PostUW": "|Area-Unicorn Way| and |Building-Rattlebones| and |Area-The Commons| and {ItemValue(damage:26)}",
+        "To Muldoon": "{wizReach(PostUW)} and |Area-Ravenwood| and |Area-Olde Town| and (|Area-Shopping District| or |Teleport-Friendly|)",
+        "Judd": "{wizReach(To Muldoon)} and |Building-Judd| and |Slot-Pet| and |Slot-Mount|",
+        "Golem Court": "|Area-Golem Court| and ({wizReach(PostUW)} or |Teleport-Friendly|)",
+        "Shopping District": "|Area-Shopping District| and ({wizReach(PostUW)} or |Teleport-Majid| or (|Area-Olde Town| and |Teleport-Friendly|))",
+        "Apples": "|Area-The Commons| and {wizReach(Golem Court)} and {wizReach(Shopping District)}" # to collapse the very lengthy logic for the second half of the Ghosts/Apple questline
+    }
+    return "(" + locations_dict[location] + ")" # not wrapping these strings in parentheses can break logic in subtle ways
+    
 # Sometimes you have a requirement that is just too messy or repetitive to write out with boolean logic.
 # Define a function here, and you can use it in a requires string with {function_name()}.
 def overfishedAnywhere(world: World, state: CollectionState, player: int):
