e9b24489908f84b4edec7760713264d15ab7616d
Andrew Sonnier <absonnie@ncsu.edu>
2026-01-18 22:01:48 -0500

    Updated manual to unstable 20251205

--- manual_wizard101_cloiss/hooks/Helpers.py
+++ manual_wizard101_cloiss/hooks/Helpers.py
@@ -1,10 +1,6 @@
-from typing import Optional, TYPE_CHECKING
+from typing import Optional, Any
 from BaseClasses import MultiWorld, Item, Location
 from worlds.AutoWorld import World
-
-if TYPE_CHECKING:
-    from ..Items import ManualItem
-    from ..Locations import ManualLocation
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
@@ -13,10 +9,10 @@ def before_is_category_enabled(multiworld: MultiWorld, player: int, category_nam
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the item, False to disable it, or None to use the default behavior
-def before_is_item_enabled(multiworld: MultiWorld, player: int, item: "ManualItem") -> Optional[bool]:
+def before_is_item_enabled(multiworld: MultiWorld, player: int, item: dict[str, Any]) -> Optional[bool]:
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the location, False to disable it, or None to use the default behavior
-def before_is_location_enabled(multiworld: MultiWorld, player: int, location: "ManualLocation") -> Optional[bool]:
+def before_is_location_enabled(multiworld: MultiWorld, player: int, location: dict[str, Any]) -> Optional[bool]:
     return None
