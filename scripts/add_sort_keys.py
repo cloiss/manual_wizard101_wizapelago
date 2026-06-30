9fb07dd03c235465b53d17c32033c0acd9731f8e
ItzGray <masonmustonen@gmail.com>
2026-06-30 07:51:22 -0500

    Update quest sorting

--- scripts/add_sort_keys.py
+++ scripts/add_sort_keys.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 def main():
     # Path to locations.json relative to the script location
@@ -9,6 +10,10 @@ def main():
     if not os.path.exists(file_path):
         # Fallback if run from inside the manual_wizard101_cloiss folder
         file_path = os.path.join(script_dir, "data", "locations.json")
+    
+    if not os.path.exists(file_path):
+        # Fallback if run from inside the scripts folder
+        file_path = os.path.join(Path(script_dir).parent, "manual_wizard101_cloiss", "data", "locations.json")
         
     if not os.path.exists(file_path):
         print("Error: Could not locate locations.json.")
@@ -19,6 +24,10 @@ def main():
         content = json.load(f)
 
     updated_count = 0
+    last_category = None
+    current_category = None
+    keys = "abcdefghijklmnopqrstuvwxyz"
+    shift = 0
     for location in content.get("data", []):
         if "log_msg" not in location:
             # Prefixing with an underscore "_" sorts them before letters A-Z / a-z 
@@ -26,9 +35,14 @@ def main():
             location["sort-key"] = "_" + location["name"]
             updated_count += 1
         else:
-            # Clean up sort-key on auto-completed locations to let them sort normally
-            if "sort-key" in location:
-                del location["sort-key"]
+            current_category = location["category"][0]
+            if current_category != last_category:
+                shift = 0
+                last_category = current_category
+            location["sort-key"] = keys[shift] + location["name"]
+            shift += 1
+            updated_count += 1
+        print(f"{location['sort-key']}, {location['category']}")
 
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(content, f, indent=4, ensure_ascii=False)
