047a94810229a5a5517a217eb0f0eb5e81ea84b3
cloiss <epikcloiss37@gmail.com>
2026-07-02 17:30:40 -0500

    change keys to numerical system 000-999, add sorting to items.json

--- scripts/add_sort_keys.py
+++ scripts/add_sort_keys.py
@@ -2,18 +2,18 @@ import json
 import os
 from pathlib import Path
 
-def main():
+def update_json(file_name: str):
     # Path to locations.json relative to the script location
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_dir, "manual_wizard101_cloiss", "data", "locations.json")
+    file_path = os.path.join(script_dir, "manual_wizard101_cloiss", "data", file_name)
     
     if not os.path.exists(file_path):
         # Fallback if run from inside the manual_wizard101_cloiss folder
-        file_path = os.path.join(script_dir, "data", "locations.json")
+        file_path = os.path.join(script_dir, "data", file_name)
     
     if not os.path.exists(file_path):
         # Fallback if run from inside the scripts folder
-        file_path = os.path.join(Path(script_dir).parent, "manual_wizard101_cloiss", "data", "locations.json")
+        file_path = os.path.join(Path(script_dir).parent, "manual_wizard101_cloiss", "data", file_name)
         
     if not os.path.exists(file_path):
         print("Error: Could not locate locations.json.")
@@ -26,10 +26,9 @@ def main():
     updated_count = 0
     last_category = None
     current_category = None
-    keys = "abcdefghijklmnopqrstuvwxyz"
-    shift = 0
+    counter = "000"
     for location in content.get("data", []):
-        if "log_msg" not in location:
+        if ("log_msg" not in location) and file_name == "locations.json":
             # Prefixing with an underscore "_" sorts them before letters A-Z / a-z 
             # in both case-sensitive (Custom) and case-insensitive (Natural) sort orders.
             location["sort-key"] = "_" + location["name"]
@@ -37,17 +36,24 @@ def main():
         else:
             current_category = location["category"][0]
             if current_category != last_category:
-                shift = 0
+                counter = "000"
                 last_category = current_category
-            location["sort-key"] = keys[shift] + location["name"]
-            shift += 1
+            location["sort-key"] = counter + location["name"]
+            counter = increment(counter)
             updated_count += 1
         print(f"{location['sort-key']}, {location['category']}")
 
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(content, f, indent=4, ensure_ascii=False)
 
-    print(f"Successfully updated {updated_count} manual locations in locations.json")
+    print(f"Successfully updated {updated_count} manual locations in {file_name}")
+
+def increment(num: str):
+    num = str(int(num)+1)
+    while len(num) < 3:
+        num = "0" + num
+    return num
 
 if __name__ == "__main__":
-    main()
+    update_json("locations.json")
+    update_json("items.json")
